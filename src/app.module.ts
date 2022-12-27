43a7725904f285136202f4ecb288e3a7e2977482
alex <choijeaho86@gmail.com>
2022-12-28 06:01:50 +0900

    custom env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
       // env파일 스키마 점검
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.required(), // env파일에 DATABASE_HOST가 반드시 있어야함
