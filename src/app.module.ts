c09d1fefd80455b2b8e3c692c87e5f99ef239261
alex <choijeaho86@gmail.com>
2022-12-28 05:32:33 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+// src/app.module.ts
+import * as Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      // env파일 스키마 점검
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(), // env파일에 DATABASE_HOST가 반드시 있어야함
+        DATABASE_PORT: Joi.number().default(3305), // env파일에 DATABASE_PORT가 있어야 하지만 없으면 임의로 3305입력
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
